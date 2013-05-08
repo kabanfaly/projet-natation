e24da39b054a549bb88140379e0c332729d73e1a
nfalykaba@gmail.com <nfalykaba@gmail.com@3a2eb54b-0dba-660f-6953-7e2770e98593>
2013-05-08 19:05:25 +0000

    ajout de l'autoincrementation aux ids

--- natation/model/class.performance.php
+++ natation/model/class.performance.php
@@ -8,5 +8,60 @@
 include_once '../include/connexion.php';
 class performance {
     
+    /**
+     * Nom de la cle primaire de la table
+     * @var string 
+     */
+    private $cle_primaire = 'idperformance';
+    /**
+     * Nom de la table
+     * @var string 
+     */
+    private $table = 'performance';
+    /**
+     * Le nombre de points
+     * @var int 
+     */
+    private $points;
+    /**
+     * Le temps (chrono)
+     * @var string
+     */
+    private $temps;
+    /**
+     * L'id du nageur
+     * @var int 
+     */
+    private $id_nageur;
+    /**
+     * L'id de l'epreuve
+     * @var int 
+     */
+    private $id_epreuve;
+            
+    function __construct($points, $temps, $id_nageur, $id_epreuve) {
+        $this->points = $points;
+        $this->temps = $temps;
+        $this->id_nageur = $id_nageur;
+        $this->id_epreuve = $id_epreuve;
+    }
+    
+    /**
+     * 
+     * @return boolean
+     * @throws Exception
+     */
+    public function enregistrer() {
+        //Recherche si l'element a enregistrer existe dans la table courante
+        $res = mysql_query("SELECT * FROM `$this->table` WHERE `type` =  '$this->type'") or die(mysql_error());
+        
+        //Exception si le type existe 
+        if (mysql_num_rows($res) != 0) {
+            throw new Exception("Ce type existe deja");
+        }
+        //si le type n'existe pas on l'enregistre
+        mysql_query("INSERT INTO `$this->table` (`type`) VALUES ('$this->type')") or die(mysql_error());
+        return true;
+    }
 }
 
--- natation/model/class.type_nage.php
+++ natation/model/class.type_nage.php
@@ -6,7 +6,64 @@
  * @author kaba
  */
 include_once '../include/connexion.php';
+
 class type_nage {
-    
+
+    /**
+     * Le type de nage
+     * @var string
+     */
+    private $type;
+    /**
+     * Nom de la table dans la base de donnees
+     * @var string 
+     */
+    private $table = 'type_de_nage';
+    /**
+     * Nom de la cle primaire de la table
+     * @var string 
+     */
+    private $cle_primaire = 'id_type_de_nage';
+
+    function __construct($type) {
+        $this->type = $type;
+    }
+
+    /**
+     * Enregistre un type de nage s'il n'existe pas
+     * @param string $type le type a enregistrer
+     * @return boolean true si enregistrement ok
+     * @throws Exception si le type existe deja
+     */
+    public function enregistrer() {
+        //Recherche si l'element a enregistrer existe dans la table courante
+        $res = mysql_query("SELECT * FROM `$this->table` WHERE `type` =  '$this->type'") or die(mysql_error());
+        
+        //Exception si le type existe 
+        if (mysql_num_rows($res) != 0) {
+            throw new Exception("Ce type existe deja");
+        }
+        //si le type n'existe pas on l'enregistre
+        mysql_query("INSERT INTO `$this->table` (`type`) VALUES ('$this->type')") or die(mysql_error());
+        return true;
+    }
+    /**
+     * Modifie le type courant
+     * @param string $nouveau_type le nouveau type
+     * @return boolean true si modification OK
+     */
+    public function modifier($nouveau_type){
+        mysql_query("UPDATE `$this->table` SET `type` = '$nouveau_type' WHERE `type` = '$this->type'") or die(mysql_error());
+        return true;
+    }
+
+    /**
+     * Supprime un type par son ID
+     * @param int $idType
+     */
+    public static function  supprimerParId($idType){
+        mysql_query("DELELE FROM `$this->type` WHERE `$this->cle_primaire` = $idType") or die(mysql_error());
+        return true;
+    }
 }
 
