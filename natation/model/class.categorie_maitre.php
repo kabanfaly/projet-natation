b347113ad700f30335a246b8e3a0e381f2f0059d
nfalykaba@gmail.com <nfalykaba@gmail.com@3a2eb54b-0dba-660f-6953-7e2770e98593>
2013-05-09 14:30:51 +0000

    implémentation des classes modèles

--- natation/model/class.categorie_maitre.php
+++ natation/model/class.categorie_maitre.php
@@ -7,6 +7,111 @@
  */
 include_once '../include/connexion.php';
 class categorie_maitre {
-    //put your code here
+
+    /**
+     * Nom de la table dans la base de donnees
+     * @var string 
+     */
+    private static $table = 'categorie_maitre';
+
+    /**
+     * Nom de la colonne cle primaire de la table
+     * @var string 
+     */
+    private static $cle_primaire = 'idcategorie_maitre';
+
+    function __construct() {
+        
+    }
+
+    /**
+     * Enregistre une categorie de maitre s'il n'existe pas
+     * @param string $categorie  la categorie a enregistrer
+     * @return boolean true si enregistrement ok
+     * @throws Exception si le type existe deja
+     */
+    public static function enregistrer($categorie) {
+        //Recherche si l'element a enregistrer existe dans la table courante
+        $res = mysql_query("SELECT * FROM `" . self::$table . "` WHERE `categorie` =  '$categorie'") or die(mysql_error());
+
+        //Exception si la categorie existe 
+        if (mysql_num_rows($res) != 0) {
+            throw new Exception("Ce categorie existe deja");
+        }
+        //si le type n'existe pas on l'enregistre
+        mysql_query("INSERT INTO `" . self::$table . "` (`categorie`) VALUES ('$categorie')") or die(mysql_error());
+        return true;
+    }
+
+    /**
+     * Modifie la categorie courant suivant l'id 
+     * @param int $idtype_nage l'id du type
+     * @param string $nouvelle_categorie la  nouvelle categorie
+     * @return boolean true si modification OK
+     */
+    public static function modifier($id_categorie_maitre, $nouvelle_categorie) {
+        mysql_query("UPDATE `" . self::$table . "` SET `type` = '$nouvelle_categorie' WHERE `" . self::$cle_primaire . "` = '$id_categorie_maitre'") or die(mysql_error());
+        return true;
+    }
+
+    /**
+     * Supprime une categorie par son ID
+     * @param int $id_categorie_maitre
+     */
+    public static function supprimer($id_categorie_maitre) {
+        mysql_query("DELELE FROM `" . self::$table . "` WHERE `" . self::$cle_primaire . "` = $id_categorie_maitre") or die(mysql_error());
+        return true;
+    }
+
+   /**
+     * Recherche une categorie de maitre par son id
+     * @param int $id_categorie_maitre l'id de la categorie de maitre
+     * @return boolean | array, false si aucune categorie correspondante n'a ete trouvée, sinon retourne la ligne correspondante à la categorie de maitre
+     */
+    public static function rechercherParId($id_categorie_maitre) {
+        $res = mysql_query("SELECT * FROM `" . self::$table . "` WHERE `".self::$cle_primaire."` =  '$id_categorie_maitre'") or die(mysql_error());
+        if (mysql_num_rows($res) != 0) {
+            return mysql_fetch_array($res);
+        }
+        return false;
+    }
+    /**
+     * Recherche par  categorie de maitre
+     * @param string $categorie la categorie à rechercher
+     * @return boolean | array,  false si la categorie n'a pas ete trouvée ou toutes les categories de maitre correspondant à la categorie de maitre
+     */
+    public static function rechercherParCategorie($categorie) {
+        $res = mysql_query("SELECT * FROM `" . self::$table . "` WHERE `categorie` LIKE  '%$categorie%'") or die(mysql_error());
+        if (mysql_num_rows($res) != 0) {
+            $categories = array();
+            //Recuperer toutes les lignes trouvees
+            while (($ligne = mysql_fetch_array($res)) !== FALSE) {
+                $categories[] = $ligne;
+            }
+            return $categories;
+        }
+        return false;
+    }
+
+    /**
+     * Recherche toutes les categories
+     * @return boolean | array
+     */
+    public static function rechercherTout() {
+        //Recuperer toutes les categories
+        $res = mysql_query("SELECT * FROM `" . self::$table . "`") or die(mysql_error());
+        if (mysql_num_rows($res) != 0) {
+            $types = array();
+            //Recuperer toutes les lignes trouvees
+            while (($ligne = mysql_fetch_array($res)) !== FALSE) {
+                $categories[] = $ligne;
+            }
+            return $categories;
+        }
+        return false;
+    }
+
 }
+
+
 
--- natation/model/class.competition.php
+++ natation/model/class.competition.php
@@ -7,5 +7,163 @@
  */
 include_once '../include/connexion.php';
 class competition {
-    //put your code here
+
+    /**
+     * Nom de la colonne cle primaire de la table
+     * @var string 
+     */
+    private static $cle_primaire = 'idcompetition';
+
+    /**
+     * Nom de la table
+     * @var string 
+     */
+    private static $table = 'competition';
+
+
+    function __construct() {
+    }
+
+    /**
+     * Ajoute une nouvelle competition
+     * @return boolean
+     * @throws Exception
+     */
+    public static function enregistrer($annee, $idcategorie_maitre, $id_nageur, $id_epreuve) {
+        mysql_query("INSERT INTO `".self::$table."` (`annee`, `idcategorie_maitre`, `idnageur`, `idepreuve`) VALUES "
+                        . "('$annee', '$idcategorie_maitre', '$id_nageur', '$id_epreuve')") or die(mysql_error());
+        return true;
+    }
+
+    /**
+     * Modification d'une competition suivant l'id
+     * @param type $idperformance
+     * @param type $annee
+     * @param type $idcategorie_maitre
+     * @param type $id_nageur
+     * @param type $id_epreuve
+     * @return boolean
+     */
+    public static function modifier($idcompetition, $annee, $idcategorie_maitre, $id_nageur, $id_epreuve) {
+        mysql_query("UPDATE `".self::$table."` SET `annee` = '$annee', `idcategorie_maitre` = '$idcategorie_maitre', `idnageur` = '$id_nageur', `idepreuve` = '$id_epreuve'"
+                        . " WHERE `".self::$cle_primaire."` = '$idcompetition'") or die(mysql_error());
+        return true;
+    }
+
+    /**
+     * Supprime une competition
+     * @param type $id_competition
+     */
+    public static function supprimer($id_competition) {
+        mysql_query("DELELE FROM `".self::$table."` WHERE `".self::$cle_primaire."` = $id_competition") or die(mysql_error());
+        return true;
+    }
+
+    /**
+     * Recherche les competitions  par son id
+     * @param int $id_competition l'id de la performance
+     * @return boolean | array, false si aucune competition correspondante n'a ete trouvee, sinon retourne la ligne correspondante
+     * a la competition
+     */
+    public static function rechercherParId($id_competition) {
+        $res = mysql_query("SELECT * FROM `".self::$table."` WHERE `".self::$cle_primaire."` =  '$id_competition'") or die(mysql_error());
+        if (mysql_num_rows($res) != 0) {           
+            return mysql_fetch_array($res);
+        }
+        return false;
+    }
+    /**
+     * Recherche les competitions d'un nageur
+     * @param int $id_nageur l'id du nageur
+     * @return boolean | array, false si aucune performance correspondante n'a ete trouvee, sinon retourne les competitions du nageur 
+     * dans un tableau
+     */
+    public static function rechercherParNageur($id_nageur) {
+        $res = mysql_query("SELECT * FROM `".self::$table."` WHERE `idnageur` =  '$id_nageur'") or die(mysql_error());
+        if (mysql_num_rows($res) != 0) {
+            $competitions = array();
+            //Recuperer toutes les lignes trouvées
+            while (($ligne = mysql_fetch_array($res)) !== FALSE) {
+                $competitions[] = $ligne;
+            }
+            return $competitions;
+        }
+        return false;
+    }
+    /**
+     * Recherche les competitions d'une categorie
+     * @param int $id_categorie_maitre l'id de categorie_maitre
+     * @return boolean | array, false si aucune performance correspondante n'a ete trouvee, sinon retourne les competitions de la categorie_maitre
+     * dans un tableau
+     */
+    public static function rechercherParcategorie_maitre($idcategorie_maitre) {
+        $res = mysql_query("SELECT * FROM `".self::$table."` WHERE `$idcategorie_maitre` =  '$idcategorie_maitre'") or die(mysql_error());
+        if (mysql_num_rows($res) != 0) {
+            $competitions = array();
+            //Recuperer toutes les lignes trouvées
+            while (($ligne = mysql_fetch_array($res)) !== FALSE) {
+                $competitions[] = $ligne;
+            }
+            return $competitions;
+        }
+        return false;
+    }
+    
+    /**
+     * Recherche les competitions d'une epreuve
+     * @param int $idepreuve l'id de l'epreuve
+     * @return boolean | array, false si aucune competition correspondante n'a ete trouvee, sinon retourne les competitions du nageur 
+     * dans un tableau
+     */
+    public static function rechercherParEpreuve($idepreuve) {
+        $res = mysql_query("SELECT * FROM `".self::$table."` WHERE `idepreuve` =  '$idepreuve'") or die(mysql_error());
+        if (mysql_num_rows($res) != 0) {
+            $competitions = array();
+            //Recuperer toutes les lignes trouves
+            while (($ligne = mysql_fetch_array($res)) !== FALSE) {
+                $competitions[] = $ligne;
+            }
+            return $competitions;
+        }
+        return false;
+    }
+    /**
+     * Recherche les competitions d'un nageur pour une epreuve donnee
+     * @param int $idnageur l'id du nageur
+     * @param int $idepreuve l'id de l'epreuve
+     * @return boolean | array, false si aucune competitioncorrespondante n'a ete trouvee suivant l'epreuve, sinon retourne les competitions du nageur 
+     * dans un tableau
+     */
+    public static function rechercherParNageurEpreuve($idnageur, $idepreuve){
+        $res = mysql_query("SELECT * FROM `".self::$table."` WHERE `idnageur` =  '$idnageur' AND `idepreuve` =  '$idepreuve'") or die(mysql_error());
+        if (mysql_num_rows($res) != 0) {
+            $competitions = array();
+            //Recuperer toutes les lignes trouves
+            while (($ligne = mysql_fetch_array($res)) !== FALSE) {
+                $competitions[] = $ligne;
+            }
+            return $competitions;
+        }
+        return false;
+    }
+    
+    /**
+     * Recherche toutes les competitions
+     * @return boolean | array
+     */
+    public static function rechercherTout(){
+         //Recuperer toutes les competitions
+        $res = mysql_query("SELECT * FROM `".self::$table."`") or die(mysql_error());
+        if (mysql_num_rows($res) != 0) {
+            $competitions = array();
+            //Recuperer toutes les lignes trouvees
+            while (($ligne = mysql_fetch_array($res)) !== FALSE) {
+                $competitions[] = $ligne;
+            }
+            return $competitions;
+        }
+        return false;
+    }
 }
+
+
