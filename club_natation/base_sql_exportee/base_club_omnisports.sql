a4ae2ef515a112c0907e6ff627498bce1381900f
nfalykaba@gmail.com <nfalykaba@gmail.com@3a2eb54b-0dba-660f-6953-7e2770e98593>
2013-06-03 22:32:57 +0000

    bd reorganisation

--- club_natation/base_sql_exportee/base_club_omnisports.sql
+++ club_natation/base_sql_exportee/base_club_omnisports.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 3.4.10.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mar 21 Mai 2013 à 15:33
--- Version du serveur: 5.5.24-log
--- Version de PHP: 5.3.13
+-- Généré le : Mar 04 Juin 2013 à 00:25
+-- Version du serveur: 5.5.31
+-- Version de PHP: 5.3.10-1ubuntu3.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données: `base club omnisports`
@@ -26,6 +20,7 @@ SET time_zone = "+00:00";
 -- Structure de la table `categorie`
 --
 
+DROP TABLE IF EXISTS `categorie`;
 CREATE TABLE IF NOT EXISTS `categorie` (
   `id_categorie` int(11) NOT NULL AUTO_INCREMENT,
   `nom_categorie` varchar(50) NOT NULL,
@@ -53,6 +48,7 @@ INSERT INTO `categorie` (`id_categorie`, `nom_categorie`, `description`, `sexe`)
 -- Structure de la table `categorie_maitre`
 --
 
+DROP TABLE IF EXISTS `categorie_maitre`;
 CREATE TABLE IF NOT EXISTS `categorie_maitre` (
   `id_categ_maitre` int(11) NOT NULL AUTO_INCREMENT,
   `nom_categ_maitre` varchar(50) NOT NULL,
@@ -83,6 +79,7 @@ INSERT INTO `categorie_maitre` (`id_categ_maitre`, `nom_categ_maitre`, `descript
 -- Structure de la table `chrono`
 --
 
+DROP TABLE IF EXISTS `chrono`;
 CREATE TABLE IF NOT EXISTS `chrono` (
   `Num_detail_perf` int(11) NOT NULL AUTO_INCREMENT,
   `num_perf` int(11) NOT NULL,
@@ -96,6 +93,7 @@ CREATE TABLE IF NOT EXISTS `chrono` (
 -- Structure de la table `epreuve`
 --
 
+DROP TABLE IF EXISTS `epreuve`;
 CREATE TABLE IF NOT EXISTS `epreuve` (
   `id_epreuve` int(11) NOT NULL AUTO_INCREMENT,
   `type_epreuve` varchar(50) NOT NULL,
@@ -133,6 +131,7 @@ INSERT INTO `epreuve` (`id_epreuve`, `type_epreuve`, `distance_epreuve`) VALUES
 -- Structure de la table `nageur`
 --
 
+DROP TABLE IF EXISTS `nageur`;
 CREATE TABLE IF NOT EXISTS `nageur` (
   `id_nageur` int(11) NOT NULL AUTO_INCREMENT,
   `nom_nageur` varchar(50) NOT NULL,
@@ -141,17 +140,18 @@ CREATE TABLE IF NOT EXISTS `nageur` (
   `annee_naissance` date NOT NULL,
   `age` int(11) NOT NULL,
   `id_categorie` int(11) NOT NULL,
-  `id_categorie_maitre` int(11) NOT NULL,
   `club` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_nageur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`id_nageur`),
+  KEY `id_categorie` (`id_categorie`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `nageur`
 --
 
-INSERT INTO `nageur` (`id_nageur`, `nom_nageur`, `prenom_nageur`, `sexe`, `annee_naissance`, `age`, `id_categorie`, `id_categorie_maitre`, `club`) VALUES
-(1, 'COULOT', 'Christian', 'Homme', '0000-00-00', 0, 0, 2, 'CO Vernouillet');
+INSERT INTO `nageur` (`id_nageur`, `nom_nageur`, `prenom_nageur`, `sexe`, `annee_naissance`, `age`, `id_categorie`, `club`) VALUES
+(1, 'COULOT', 'Christian', 'Homme', '0000-00-00', 0, 1, 'CO Vernouillet'),
+(2, 'KABA', 'N''faly', 'Homme', '1984-12-20', 0, 1, '');
 
 -- --------------------------------------------------------
 
@@ -159,25 +159,33 @@ INSERT INTO `nageur` (`id_nageur`, `nom_nageur`, `prenom_nageur`, `sexe`, `annee
 -- Structure de la table `performance`
 --
 
+DROP TABLE IF EXISTS `performance`;
 CREATE TABLE IF NOT EXISTS `performance` (
-  `Num_perf` int(11) NOT NULL AUTO_INCREMENT,
+  `id_performance` int(11) NOT NULL AUTO_INCREMENT,
   `id_nageur` int(11) NOT NULL,
   `id_epreuve` int(11) NOT NULL,
   `saison` year(4) NOT NULL,
   `date_perf` date NOT NULL,
   `temps_total` time NOT NULL,
   `points` int(11) NOT NULL,
-  PRIMARY KEY (`Num_perf`)
+  PRIMARY KEY (`id_performance`),
+  KEY `id_epreuve` (`id_epreuve`),
+  KEY `id_nageur` (`id_nageur`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Contenu de la table `performance`
+-- Contraintes pour les tables exportées
 --
 
-INSERT INTO `performance` (`Num_perf`, `id_nageur`, `id_epreuve`, `saison`, `date_perf`, `temps_total`, `points`) VALUES
-(1, 1, 0, 0000, '0000-00-00', '00:06:37', 423),
-(2, 0, 0, 0000, '0000-00-00', '00:00:00', 0);
+--
+-- Contraintes pour la table `nageur`
+--
+ALTER TABLE `nageur`
+  ADD CONSTRAINT `nageur_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Contraintes pour la table `performance`
+--
+ALTER TABLE `performance`
+  ADD CONSTRAINT `performance_ibfk_2` FOREIGN KEY (`id_epreuve`) REFERENCES `epreuve` (`id_epreuve`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `performance_ibfk_1` FOREIGN KEY (`id_nageur`) REFERENCES `nageur` (`id_nageur`) ON DELETE CASCADE ON UPDATE NO ACTION;
---
+++ club_natation/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link type="text/css" href="style.css" rel="stylesheet" />
+        <title>Club de natation</title>
+    </head>
+    <body>
+        <div id="principal">
+            
+        </div>
+    </body>
+</html>
