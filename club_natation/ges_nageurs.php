0179cb921859438d7b20dcff2c5f8c566379cc70
nfalykaba@gmail.com <nfalykaba@gmail.com@3a2eb54b-0dba-660f-6953-7e2770e98593>
2013-06-06 10:28:38 +0000

    creation header et footer

--- club_natation/ges_nageurs.php
+++ club_natation/ges_nageurs.php
@@ -1,17 +1,6 @@
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php include 'header.php'; ?>
+<div id="contenu">
+    <h2>Gestion des nageurs</h2>
+    
+</div>
+<?php include 'footer.php'; ?>
--- club_natation/index.php
+++ club_natation/index.php
@@ -1,48 +1,5 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link type="text/css" href="style.css" rel="stylesheet" />
-        <title>Club de natation</title>
-    </head>
-    <body>
-        <div id="principal">
-            <div id="header">
-                <div id="blank"></div>
-                <span id="titre">Club Omnisports Vernouillet</span>
-            </div>
-            <div id="menuHoriz">
-                <ul>
-                    <?php
-                    if (isset($_SESSION['connexionOK']) && $_SESSION['connexionOK']) {
-                        ?>
-                        <li><a href="quitter.php">Déconnexion</a></li>
-                        <?php
-                    } else {
-                        ?>
-                        <li><a href="connexion.php">Connexion</a></li>
-                        <?php
-                    }
-                    ?>
-                </ul>
-            </div>
-            <div id="contenuCentral">
-                <div id="menuVert"> 
-                    <div id="bloc1">
-                        <div id="nav1"><a href="nageurs.php">Liste des nageurs</a></div>
-                    </div>
-                    <div id="bloc2">
-                        <div id="nav2"><a href="ges_nageurs.php">Gestion des nageurs</a></div>
-                        <div id="nav2"><a href="ges_categories.php">Gestion des catégories</a></div>
-                        <div id="nav2"><a href="ges_epreuves.php">Gestion des épreuves</a></div>
-                        <div id="nav2"><a href="ges_performances.php">Gestion des performances</a></div>
-                    </div>
-                </div>
-                <div id="contenu">
+<?php include 'header.php'; ?>
+<div id="contenu">
 
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+</div>
+<?php include 'footer.php'; ?>
