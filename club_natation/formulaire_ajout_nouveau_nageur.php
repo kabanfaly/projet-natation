5d83bf7eaad728f721870bf75f5819e2d3550aef
nfalykaba@gmail.com <nfalykaba@gmail.com@3a2eb54b-0dba-660f-6953-7e2770e98593>
2013-06-06 11:55:39 +0000

    formulaire nageur

--- club_natation/formulaire_ajout_nouveau_nageur.php
+++ club_natation/formulaire_ajout_nouveau_nageur.php
@@ -1,61 +1,65 @@
-<html>
-    <head>
-        <title>Ajout d'une nouveau nageur</title>
-        <meta charset="utf-8"/>
-    <h1 align="center">Ajouter un nouveau nageur</h1>
-</head>
-<body>
-    <?php
-    include_once("code_connexion_pdo.php");
+<?php
+include 'header.php';
+include_once("code_connexion_pdo.php");
 
-    if ($_POST) {
-        $errors = "";
-        if ($_POST["nom_nageur"] == "")
-            $errors = $errors . "<p class='erreur'>champ nom invalide</p>";
-        if ($_POST["prenom_nageur"] == "")
-            $erros = $errors . "<p class='erreur'>champ prenom invalide</p>";
-        if (!(isset($_POST["sexe"])))
-            $erros = $errors . "<p class='erreur'>Veuillez sélectionner un sexe</p>";
+$message = "";
+if ($_POST) {
 
-        if ($_POST["annee_naissance"]) {
-            //on crée un tableau en utilisant la fonction explode qui transforme les données en tableau
-            $date = explode("/", $_POST["annee_naissance"]);
-            if (!(checkdate($date[1], $date[0], $date[2])))
-                $errors.="<p class='erreur'>année de naissance invalide</p>";
-        }
-        else {
-            $errors.="<p class='erreur'>année de naissance invalide</p>";
-        }
-        if ($errors == "") {
-            $date_naissance = $date[2] . "-" . $date[1] . "-" . $date[0];
-            $requete = "INSERT INTO nageur (`nom_nageur`,`prenom_nageur`,`sexe`,`annee_naissance`,`age`,`id_categorie`,`id_categorie_maitre`,`club`) VALUES('" . mysql_escape_string($_POST['nom_nageur']) . "','" . 
-                    mysql_escape_string($_POST['prenom_nageur'])  . "','" . $_POST['sexe'] . "','" . $date_naissance . "', '" . mysql_escape_string($_POST['age']) . "'," . mysql_escape_string($_POST['id_categorie']) . " ," 
-                    . mysql_escape_string($_POST['id_categorie_maitre']) . " , '" . mysql_escape_string($_POST['club']) . "')";
-            
-            mysql_query($requete) or die(mysql_error());
-            echo 'Enregistrement OK';
-        } else {
-            echo $errors;
-        }
+    if (!(isset($_POST["sexe"]))) {
+        $message = "<p class='erreur'>Veuillez sélectionner un sexe</p>";
     }
-    ?>
+    //on crée un tableau en utilisant la fonction explode qui transforme les données en tableau
+    $date = explode("/", $_POST["annee_naissance"]);
+    if (!(checkdate($date[1], $date[0], $date[2]))) {
+        $message = "<p class='erreur'>année de naissance invalide</p>";
+    } else {
+        $message.="<p class='erreur'>année de naissance invalide</p>";
+    }
+    if ($message == "") {
+        $date_naissance = $date[2] . "-" . $date[1] . "-" . $date[0];
+        $requete = "INSERT INTO nageur (`nom_nageur`,`prenom_nageur`,`sexe`,`annee_naissance`,`age`,`id_categorie`,`club`) VALUES('" . mysql_escape_string($_POST['nom_nageur']) . "','" .
+                mysql_escape_string($_POST['prenom_nageur']) . "','" . $_POST['sexe'] . "','" . $date_naissance . "', '" . mysql_escape_string($_POST['age']) . "'," . mysql_escape_string($_POST['id_categorie']) . " ,"
+                . mysql_escape_string($_POST['club']) . "')";
 
-
+        mysql_query($requete) or die(mysql_error());
+        $message = 'Enregistrement OK';
+    }
+}
+?>
+<div id="contenu">
+    <h2>Gestion des nageurs</h2>
+    <div><?= $message ?></div>
     <form method="post">
-
-        <p>Nom: <input type="text" name="nom_nageur" ></p>
-        <p>Prénom: <input type="text" name="prenom_nageur" ></p>
-        <p>Sexe: H <input type="radio" name="sexe" value="Homme"> F <input type="radio" name="sexe" value="Femme"></p>
-        <p>Année de naissance(au format JJ/MM/AAAA):<input type="text" name="annee_naissance" value=""></p>
-        <p>Age: <input type="text" name="age" ></p>
-        <p>ID catégorie: <input type="text" name="id_categorie" ></p>
-        <p>ID categorie maître: <input type="text" name="id_categorie_maitre" ></p>
-        <p>Club: <input type="text" name="club" ></p>
-        <p><input type="submit" name="ajouter" value="Ajouter"></p>
-
+        <table>
+            <tr>
+                <td>Nom: </td>
+                <td><input type="text" name="nom_nageur" required="true"></td>
+            </tr>
+            <tr>
+                <td>Prénom: </td>
+                <td><input type="text" name="prenom_nageur"  required="true"> </td>
+            </tr>
+            <tr>
+                <td>Sexe: </td>
+                <td>H <input type="radio" name="sexe" value="Homme"> F <input type="radio" name="sexe" value="Femme"></td>
+            </tr>
+            <tr>
+                <td>Année de naissance(au format JJ/MM/AAAA):</td>
+                <td><input type="text" name="annee_naissance" value=""  required="true"></td>
+            </tr>
+            <tr>
+                <td>Catégorie: </td>
+                <td><input type="text" name="id_categorie" ></td>
+            </tr>
+            <tr>
+                <td>Club: </td>
+                <td><input type="text" name="club"  required="true"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" name="ajouter" value="Ajouter"></td>
+            </tr>
+        </table>
     </form>
+</div>
+<?php include 'footer.php'; ?>
 
-
-
-</body>
-</html>
--- club_natation/ges_nageurs.php
+++ club_natation/ges_nageurs.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
 <div id="contenu">
     <h2>Gestion des nageurs</h2>
-    
+    <a href="formulaire_ajout_nouveau_nageur.php">Ajouter un nageur</a>
 </div>
 <?php include 'footer.php'; ?>
